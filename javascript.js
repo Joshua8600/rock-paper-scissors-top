04cac5b5aba1ecfa121e1ca3c053973ff1f604e5
Joshua8600 <105813016+Joshua8600@users.noreply.github.com>
2026-05-05 18:10:32 -0700

    Create Function to Obtain Human Player's Choice

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ Get Computer Choice
 
 1. Get whole number
 2. Define values for each choice 1-33, 34-66, 67-99
+3. return a final value for the choice
 */
 function getRandomInt() { //Get a whole number
     return parseInt((Math.random() * 100));
@@ -18,12 +19,24 @@ function assignCpuMove() { //assign values for each choice
         return "scissors";
 }
 
-function getComputerChoice() {
+function getComputerChoice() { //return a final value for the choice
     return assignCpuMove();
 }
 
 let computerChoice = getComputerChoice();
-
 // console.log(computerChoice);
-// console.log(getRandomInt());
-// console.log(assignCpuMove());
+
+/*
+Get Human Choice
+
+1. Prompt for human player to enter
+2. return a value for comparison against CPU Move
+*/
+
+function getHumanChoice() {
+    let calculateHumanChoice = prompt("Rock, paper, or scissors?", "Enter a choice."); //prompt for human player to enter
+    return calculateHumanChoice; //return a value for comparison against CPU Move
+}
+
+let humanChoice = getHumanChoice();
+// console.log(humanChoice);
